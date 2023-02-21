e1cc6f8d148262b533fb1826273b42b833f320a9
Qodestackr <winchygichu@gmail.com>
2023-02-21 12:47:06 +0300

    init: project scaffold

---
+++ webapp/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+
+})
+
+app.post('/user', (req, res) => {
+
+})
+
+app.get('/user', () => {
+
+})
+
+app.listen(5000, () => console.log('server up, @port:5000:'))
